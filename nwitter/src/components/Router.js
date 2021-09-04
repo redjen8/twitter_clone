29295cc900795d4a8e87e620105dd41e31af029a
redjen8 <seokwoo2525@gmail.com>
2021-09-04 15:30:33 +0900

    Adding Router

---
+++ nwitter/src/components/Router.js
@@ -0,0 +1,13 @@
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
+
+const AppRouter = () => {
+    return (
+        <Router>
+            <Switch>
+                <Route />
+            </Switch>
+        </Router>
+    );
+};
+
+export default AppRouter;

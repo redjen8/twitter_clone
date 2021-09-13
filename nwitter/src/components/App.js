888f9314cdd6e33dddfe1379ec9ca1c306850957
redjen8 <seokwoo2525@gmail.com>
2021-09-13 22:01:03 +0900

    firebase auth config

--- nwitter/src/components/App.js
+++ nwitter/src/components/App.js
@@ -3,14 +3,13 @@ import AppRouter from "./Router";
 import { authService } from "fbase";
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-  console.log(authService.currentUser);
+  const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser);
   return (
     <>
     <AppRouter isLoggedIn={isLoggedIn} />
     <footer>&copy; {new Date().getFullYear()} Nwitter</footer>
     </>
-  );
+  ); 
 }
 
 export default App;
--- nwitter/src/routes/Auth.js
+++ nwitter/src/routes/Auth.js
@@ -1,3 +1,17 @@
-const Auth = () => <span>Auth</span>;
+const Auth = () => {
+    return (
+        <div>
+            <form>
+                <input type="email" placeholder="Email" required />
+                <input type="password" placeholder="Password" required />
+                <input type="submit" placeholder="Log In" required />
+            </form>
+            <div>
+                <button>Continue with Google</button>
+                <button>Continue with Github</button>
+            </div>
+        </div>
+    )
+}
 
 export default Auth;

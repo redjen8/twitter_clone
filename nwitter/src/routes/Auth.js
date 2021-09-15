2d248bde6fef1abc3208bc3fbc81a49560c53411
redjen8 <seokwoo2525@gmail.com>
2021-09-15 21:18:14 +0900

    type error fixed!!

--- nwitter/src/routes/Auth.js
+++ nwitter/src/routes/Auth.js
@@ -5,7 +5,14 @@ const Auth = () => {
     const [password, setPassword] = useState("");
 
     const onChange = (event) => {
-        console.log(event.target.name);
+        const {
+            target: {name, value},
+        } = event;
+        if(name == "email") {
+            setEmail(value);
+        } else if (name === "password") {
+            setPassword(value);
+        }
     };
 
     const onSubmit = (event) => {

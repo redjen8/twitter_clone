c6dcd960e67322960414ae2c9ac953ff7768747b
redjen8 <seokwoo2525@gmail.com>
2021-09-16 20:52:43 +0900

    saving user logged in state

--- nwitter/src/components/App.js
+++ nwitter/src/components/App.js
@@ -1,12 +1,23 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import AppRouter from "./Router";
 import { authService } from "fbase";
 
 function App() {
+  const [init, setInit] = useState(false);
   const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser);
+  useEffect(() => {
+    authService.onAuthStateChanged((user) => {
+      if (user) {
+        setIsLoggedIn(user);
+      } else {
+        setIsLoggedIn(false);
+      }
+      setInit(true);
+    });
+  }, []);
   return (
     <>
-    <AppRouter isLoggedIn={isLoggedIn} />
+    {init? <AppRouter isLoggedIn={isLoggedIn} /> : "initializing.."}
     <footer>&copy; {new Date().getFullYear()} Nwitter</footer>
     </>
   ); 

606cbd999b5079be33c8599b3a010a4f63ad0c38
redjen8 <seokwoo2525@gmail.com>
2021-09-16 20:55:05 +0900

    handles with error

--- nwitter/src/routes/Auth.js
+++ nwitter/src/routes/Auth.js
@@ -5,6 +5,7 @@ const Auth = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [newAccount, setNewAccount] = useState(true);
+    const [error, setError] = useState("");
     const onChange = (event) => {
         const {
             target: {name, value},
@@ -29,7 +30,7 @@ const Auth = () => {
             }
             console.log(data);
         } catch (error) {
-            console.log(error);
+            setError(error.message);
         }
     };
 
@@ -39,6 +40,7 @@ const Auth = () => {
                 <input name="email" type="email" placeholder="Email" required value={email} onChange={onChange}/>
                 <input name="password" type="password" placeholder="Password" required value={password} onChange={onChange}/>
                 <input type="submit" value={newAccount ? "Create Account" : "Log in"}/>
+                {error}
             </form>
             <div>
                 <button>Continue with Google</button>

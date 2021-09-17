dd68b50bbb031cbcc263bfb684b2f93f2da1b6bd
redjen8 <seokwoo2525@gmail.com>
2021-09-17 22:24:18 +0900

    added social login

--- nwitter/src/routes/Auth.js
+++ nwitter/src/routes/Auth.js
@@ -1,4 +1,4 @@
-import { authService } from "fbase";
+import { authService, firebaseInstance } from "fbase";
 import { useState } from "react";
 
 const Auth = () => {
@@ -35,6 +35,20 @@ const Auth = () => {
     };
     const toggleAccount = () => setNewAccount((prev) => !prev);
 
+    const onSocialClick = async (event) => {
+        const {
+            target: { name },
+        } = event;
+        let provider;
+        if (name == "google") {
+            provider = new firebaseInstance.auth.GoogleAuthProvider();
+        } else if (name === "github") {
+            provider = new firebaseInstance.auth.GithubAuthProvider();
+        }
+        const data = await authService.signInWithPopup(provider);
+        console.log(data);
+    };
+
     return (
         <div>
             <form onSubmit={onSubmit}>
@@ -47,8 +61,8 @@ const Auth = () => {
                 {newAccount ? "Sign In" : "Create Account"}
             </span>
             <div>
-                <button>Continue with Google</button>
-                <button>Continue with Github</button>
+                <button onClick={onSocialClick} name="google">Continue with Google</button>
+                <button onClick={onSocialClick} name="github">Continue with Github</button>
             </div>
         </div>
     )

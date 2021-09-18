11e8bb4f4370157376bb132fcb433beaa904cde1
redjen8 <seokwoo2525@gmail.com>
2021-09-18 12:24:56 +0900

    added navigation, profile router

--- nwitter/src/components/Router.js
+++ nwitter/src/components/Router.js
@@ -1,15 +1,23 @@
 import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import Auth from "routes/Auth";
 import Home from "routes/Home";
+import Profile from "routes/Profile";
+import Navigaton from "./Navigation";
 
 const AppRouter = ({ isLoggedIn }) => {
     return (
         <Router>
+            {isLoggedIn && <Navigaton/>}
             <Switch>
                 {isLoggedIn ? (
+                    <>
                     <Route exact path="/">
                         <Home />
                     </Route>
+                    <Route exact path="/profile">
+                        <Profile/>
+                    </Route>
+                    </>
                 ) : (
                     <Route exact path ="/">
                         <Auth />

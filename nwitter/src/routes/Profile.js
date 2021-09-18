cf8d2096761fc3fa0d6ef69c195dc82db67a34d7
redjen8 <seokwoo2525@gmail.com>
2021-09-18 12:27:41 +0900

    User Logout applied

--- nwitter/src/routes/Profile.js
+++ nwitter/src/routes/Profile.js
@@ -1,3 +1,13 @@
-const Profile = () => <span>Profile</span>;
+import { authService } from "fbase";
+
+const Profile = () => {
+    const onLogoutClick = () => authService.signOut();
+
+    return (
+        <>
+        <button onClick={onLogoutClick}>Log Out</button>
+        </>
+    );   
+};
 
 export default Profile;

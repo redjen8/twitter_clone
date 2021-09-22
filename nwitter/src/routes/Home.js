67c874347d6d3c726880bd3aa47fded18fed1792
redjen8 <seokwoo2525@gmail.com>
2021-09-22 20:28:22 +0900

    db read feature added

--- nwitter/src/routes/Home.js
+++ nwitter/src/routes/Home.js
@@ -1,9 +1,23 @@
  import { dbService } from "fbase";
- import { useState } from "react";
+ import { useEffect, useState } from "react";
 
 const Home = () => {
     const [nweet, setNweet] = useState("");
+    const [nweets, setNweets] = useState([]);
 
+    const getNweets = async () => {
+        const dbNweets = await dbService.collection("nweets").get();
+        dbNweets.forEach((document) => 
+            setNweets((prev) => [document.data(), ...prev])
+        );
+    };
+
+    useEffect(() => {
+        getNweets();
+    }, []);
+    
+    console.log(nweets);
+    
     const onSubmit = async (event) => {
         event.preventDefault();
         await dbService.collection("nweets").add({

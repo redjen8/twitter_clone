00b61988e5525a4536c08a1a222a65d4375a8ecc
redjen8 <seokwoo2525@gmail.com>
2021-09-25 20:33:46 +0900

    added nweet delete func

--- nwitter/src/components/Nweet.js
+++ nwitter/src/components/Nweet.js
@@ -1,10 +1,23 @@
+import { dbService } from "fbase";
+
 const Nweet = ({ nweetObj, isOwner }) => {
+    const onDeleteClick = async () => {
+        const ok = window.confirm("삭제하시겠습니까?");
+        console.log(ok);
+        if (ok) {
+            console.log(nweetObj.id);
+            const data = await dbService.doc(`nweets/${nweetObj.id}`).delete();
+            console.log(data);
+        }
+    }
+
+
     return (
         <div>
             <h4>{nweetObj.text}</h4>
             {isOwner && (
                 <>
-                <button>Delete Nweet</button>
+                <button onClick={onDeleteClick}>Delete Nweet</button>
                 <button>Edit Nweet</button>
                 </>
             )}

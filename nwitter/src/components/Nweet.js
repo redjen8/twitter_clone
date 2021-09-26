91aec737c6706c5fd8949ea965efc911723d00d0
redjen8 <seokwoo2525@gmail.com>
2021-09-26 22:25:25 +0900

    added nweet modify toggle

--- nwitter/src/components/Nweet.js
+++ nwitter/src/components/Nweet.js
@@ -1,6 +1,10 @@
 import { dbService } from "fbase";
+import { useState } from "react";
 
 const Nweet = ({ nweetObj, isOwner }) => {
+    const [editing, setEditing] = useState(false);
+    const [newNweet, setNewNweet] = useState(nweetObj.text);
+
     const onDeleteClick = async () => {
         const ok = window.confirm("삭제하시겠습니까?");
         console.log(ok);
@@ -11,17 +15,28 @@ const Nweet = ({ nweetObj, isOwner }) => {
         }
     }
 
+    const toggleEditing = () => setEditing((prev) => !prev);
 
     return (
         <div>
-            <h4>{nweetObj.text}</h4>
-            {isOwner && (
+            {editing? (
                 <>
-                <button onClick={onDeleteClick}>Delete Nweet</button>
-                <button>Edit Nweet</button>
+                <form>
+                    <input value={newNweet} required/>
+                </form>
+                <button onClick={toggleEditing}>Cancel</button>
+                </>
+            ) : (
+                <>
+                <h4>{nweetObj.text}</h4>
+                {isOwner && (
+                    <>
+                    <button onClick={onDeleteClick}>Delete Nweet</button>
+                    <button onClick={toggleEditing}>Edit Nweet</button>
+                    </>
+                )}
                 </>
             )}
-            
         </div>
     );
 };

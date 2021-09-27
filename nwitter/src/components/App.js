8516a7df2e48f7ccb9110cfafa34d92467839d2a
redjen8 <seokwoo2525@gmail.com>
2021-09-27 21:14:13 +0900

    added picture attachment

--- nwitter/src/components/App.js
+++ nwitter/src/components/App.js
@@ -21,7 +21,6 @@ function App() {
   return (
     <>
     {init? <AppRouter isLoggedIn={isLoggedIn} userObj={userObj}/> : "initializing.."}
-    <footer>&copy; {new Date().getFullYear()} Nwitter</footer>
     </>
   ); 
 }
--- nwitter/src/routes/Home.js
+++ nwitter/src/routes/Home.js
@@ -5,7 +5,7 @@
 const Home = ({userObj}) => {
     const [nweet, setNweet] = useState("");
     const [nweets, setNweets] = useState([]);
-
+    const [attachment, setAttachment] = useState("");
 
     useEffect(() => {
         dbService.collection("nweets").onSnapshot((snapshot) => {
@@ -35,6 +35,23 @@ const Home = ({userObj}) => {
         setNweet(value);
     };
 
+    const onFileChange = (event) => {
+        const {
+            target: { files },
+        } = event;
+        const theFile = files[0];
+        const reader = new FileReader();
+        reader.onloadend = (finishedEvent) => {
+            const {
+                currentTarget: { result },
+            } = finishedEvent;
+            setAttachment(result);
+        };
+        reader.readAsDataURL(theFile);
+    };
+
+    const onClearAttachment = () => setAttachment("");
+
     return (
         <>
         <form onSubmit={onSubmit}>
@@ -45,7 +62,14 @@ const Home = ({userObj}) => {
             placeholder="What's on your mind?"
             maxLength={120}
             />
+            <input type="file" accept="image/*" onChange={onFileChange}/>
             <input type="submit" value="Nweet"/>
+            {attachment && (
+                <div>
+                    <img src={attachment} width="50px" height="50px" />
+                    <button onClick={onClearAttachment}>Clear</button>
+                </div>
+            )}
         </form>
         <div>
             {nweets.map((nweet) => (

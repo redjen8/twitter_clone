d1278825069cab032c8e30c1af6406afe0043538
redjen8 <seokwoo2525@gmail.com>
2021-10-03 16:18:16 +0900

    modified profile show method as navigation

--- nwitter/src/routes/Profile.js
+++ nwitter/src/routes/Profile.js
@@ -2,27 +2,13 @@ import { authService, dbService } from "fbase";
 import { useEffect } from "react";
 import { useHistory } from "react-router-dom";
 
-const Profile = ({userObj}) => {
+const Profile = ({ userObj }) => {
     const history = useHistory();
 
     const onLogoutClick = () => {
         authService.signOut();
         history.push("/");
     };
-
-    const getMyNweets = async() => {
-        const nweets = await dbService
-        .collection("nweets")
-        .where("creatorId", "==", userObj.uid)
-        .orderBy("createdAt", "asc")
-        .get();
-
-        console.log(nweets.docs.map((doc) => doc.data()));
-    };
-
-    useEffect(() => {
-        getMyNweets();
-    }, []);
     
     return (
         <>

3fedc46b2143a8959cc75b507e592fbd1cf0bfae
redjen8 <seokwoo2525@gmail.com>
2021-10-03 16:12:54 +0900

    added collection sort and filter

--- nwitter/src/routes/Home.js
+++ nwitter/src/routes/Home.js
@@ -21,7 +21,7 @@ const Home = ({userObj}) => {
     const onSubmit = async (event) => {
         event.preventDefault();
         let attachmentUrl = "";
-        if (attachment != "") {
+        if (attachment !== "") {
             const attachmentRef = storageService
             .ref()
             .child(`${userObj.uid}/${uuidv4()}`);
--- nwitter/src/routes/Profile.js
+++ nwitter/src/routes/Profile.js
@@ -1,14 +1,29 @@
-import { authService } from "fbase";
+import { authService, dbService } from "fbase";
+import { useEffect } from "react";
 import { useHistory } from "react-router-dom";
 
-const Profile = () => {
+const Profile = ({userObj}) => {
     const history = useHistory();
 
     const onLogoutClick = () => {
         authService.signOut();
         history.push("/");
-    }
+    };
 
+    const getMyNweets = async() => {
+        const nweets = await dbService
+        .collection("nweets")
+        .where("creatorId", "==", userObj.uid)
+        .orderBy("createdAt", "asc")
+        .get();
+
+        console.log(nweets.docs.map((doc) => doc.data()));
+    };
+
+    useEffect(() => {
+        getMyNweets();
+    }, []);
+    
     return (
         <>
         <button onClick={onLogoutClick}>Log Out</button>

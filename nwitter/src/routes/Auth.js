a340d9cc75405a5184c07e13b64eefa2f388d1ad
redjen8 <seokwoo2525@gmail.com>
2021-10-07 21:55:09 +0900

    Auth Form refactored

--- nwitter/src/routes/Auth.js
+++ nwitter/src/routes/Auth.js
@@ -1,39 +1,7 @@
+import AuthForm from "components/AuthForm";
 import { authService, firebaseInstance } from "fbase";
-import { useState } from "react";
 
 const Auth = () => {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [newAccount, setNewAccount] = useState(true);
-    const [error, setError] = useState("");
-    const onChange = (event) => {
-        const {
-            target: {name, value},
-        } = event;
-        if(name === "email") {
-            setEmail(value);
-        } else if (name === "password") {
-            setPassword(value);
-        }
-    };
-
-    const onSubmit = async (event) => {
-        event.preventDefault();
-        try {
-            let data;
-            if (newAccount) {
-                // create new account
-                data = await authService.createUserWithEmailAndPassword(email, password);
-            } else {
-                //log in
-                data = await authService.signInWithEmailAndPassword(email, password);
-            }
-            console.log(data);
-        } catch (error) {
-            setError(error.message);
-        }
-    };
-    const toggleAccount = () => setNewAccount((prev) => !prev);
 
     const onSocialClick = async (event) => {
         const {
@@ -51,15 +19,7 @@ const Auth = () => {
 
     return (
         <div>
-            <form onSubmit={onSubmit}>
-                <input name="email" type="email" placeholder="Email" required value={email} onChange={onChange}/>
-                <input name="password" type="password" placeholder="Password" required value={password} onChange={onChange}/>
-                <input type="submit" value={newAccount ? "Create Account" : "Log in"}/>
-                {error}
-            </form>
-            <span onClick={toggleAccount}>
-                {newAccount ? "Sign In" : "Create Account"}
-            </span>
+            <AuthForm />
             <div>
                 <button onClick={onSocialClick} name="google">Continue with Google</button>
                 <button onClick={onSocialClick} name="github">Continue with Github</button>

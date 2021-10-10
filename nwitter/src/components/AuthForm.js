500f013168a380bd1c14886be7644f64e546dbd1
redjen8 <seokwoo2525@gmail.com>
2021-10-10 22:30:37 +0900

    auth page styled applied

--- nwitter/src/components/AuthForm.js
+++ nwitter/src/components/AuthForm.js
@@ -41,7 +41,7 @@ const AuthForm = () => {
 
     return (
         <>
-            <form onSubmit={onSubmit}>
+            <form onSubmit={onSubmit} className="container">
                 <input
                     name="email"
                     type="text"
@@ -49,6 +49,7 @@ const AuthForm = () => {
                     required
                     value={email}
                     onChange={onChange}
+                    className="authInput"
                 />
                 <input
                     name="password"
@@ -57,11 +58,16 @@ const AuthForm = () => {
                     required
                     value={password}
                     onChange={onChange}
+                    className="authInput"
                 />
-                <input type="submit" value={newAccount ? "Create Account" : "Log In"} />
-                {error}
+                <input
+                type="submit"
+                value={newAccount ? "Create Account" : "Log In"}
+                className="authInput authSubmit"
+                />
+                {error && <span className="authError">{error}</span>}
             </form>
-            <span onClick={toggleAccount}>
+            <span onClick={toggleAccount} className="authSwitch">
                 {newAccount ? "Sign In" : "Create Account"}
             </span>
         </>

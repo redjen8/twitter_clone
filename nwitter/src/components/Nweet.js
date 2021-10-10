83ce5af1a4e58e592e055399996e3fa85e2ce98d
redjen8 <seokwoo2525@gmail.com>
2021-10-10 22:52:26 +0900

    added style to Navigation, Nweet, NweetFactory

--- nwitter/src/components/Nweet.js
+++ nwitter/src/components/Nweet.js
@@ -1,5 +1,7 @@
 import { dbService, storageService } from "fbase";
 import { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrash, faPencilAlt } from "@fortawesome/free-solid-svg-icons";
 
 const Nweet = ({ nweetObj, isOwner }) => {
     const [editing, setEditing] = useState(false);
@@ -30,14 +32,21 @@ const Nweet = ({ nweetObj, isOwner }) => {
     };
 
     return (
-        <div>
+        <div className="nweet">
             {editing? (
                 <>
-                <form onSubmit={onSubmit}>
-                    <input onChange={onChange} value={newNweet} required/>
-                    <input type="submit" value="Update Nweet"/>
+                <form onSubmit={onSubmit} className="container nweetEdit">
+                    <input
+                    onChange={onChange}
+                    value={newNweet}
+                    required
+                    placeholder="Edit your nweet"
+                    autoFocus
+                    className="formInput"
+                    />
+                    <input type="submit" value="Update Nweet" className="formBtn"/>
                 </form>
-                <button onClick={toggleEditing}>Cancel</button>
+                <button onClick={toggleEditing} className="formBtn cancelBtn">Cancel</button>
                 </>
             ) : (
                 <>
@@ -46,10 +55,15 @@ const Nweet = ({ nweetObj, isOwner }) => {
                     <img src={nweetObj.attachmentUrl} width="50px" height="50px"/>
                 )}
                 {isOwner && (
-                    <>
-                    <button onClick={onDeleteClick}>Delete Nweet</button>
-                    <button onClick={toggleEditing}>Edit Nweet</button>
-                    </>
+                    <div className="nweet_actions">
+                        <span onClick={onDeleteClick}>
+                            <FontAwesomeIcon icon={faTrash}/>
+                        </span>
+                        <span onClick={toggleEditing}>
+                            <FontAwesomeIcon icon={faPencilAlt}/>
+                        </span>
+                    </div>
+                    
                 )}
                 </>
             )}
--- nwitter/src/components/NweetFactory.js
+++ nwitter/src/components/NweetFactory.js
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import { dbService, storageService } from "fbase";
 import { v4 as uuidv4 } from "uuid";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlus, faTimes } from "@fortawesome/free-solid-svg-icons";
 
 const NweetFactory = ({ userObj }) => {
 
@@ -9,6 +11,9 @@ const NweetFactory = ({ userObj }) => {
 
     const onSubmit = async (event) => {
         event.preventDefault();
+        if (nweet === "") {
+            return;
+        }
         let attachmentUrl = "";
         if (attachment !== "") {
             const attachmentRef = storageService
@@ -55,20 +60,43 @@ const NweetFactory = ({ userObj }) => {
     const onClearAttachment = () => setAttachment("");
 
     return (
-        <form onSubmit={onSubmit}>
+        <form onSubmit={onSubmit} className="factoryForm">
+            <div className="factoryInput__container">
             <input
+                className="factoryInput__input"
                 value={nweet}
                 onChange={onChange}
                 type="text"
                 placeholder="What's on your mind?"
                 maxLength={120}
             />
-            <input type="file" accept="image/*" onChange={onFileChange} />
-            <input type="submit" value="Nweet" />
+            <input type="submit" value="&rarr;" className="factoryInput__arrow"/>
+            </div>
+            <label htmlFor="attach-file" className="factoryInput__label">
+                <span>Add photos</span>
+                <FontAwesomeIcon icon={faPlus}/>
+            </label>
+            <input
+            id="attach-file"
+            type="file"
+            accept="image/*"
+            onChange={onFileChange}
+            style={{
+                opacity: 0,
+            }}
+            />
             {attachment && (
-                <div>
-                    <img src={attachment} width="50px" height="50px"/>
-                    <button onClick={onClearAttachment}>Clear</button>
+                <div className="factoryForm__attachment">
+                    <img
+                    src={attachment}
+                    style={{
+                        backgroundImage: attachment,
+                    }}
+                    />
+                    <div className="factoryForm__clear" onClick={onClearAttachment}>
+                        <span>Remove</span>
+                        <FontAwesomeIcon icon={faTimes}/>
+                    </div>
                 </div>
             )}
         </form>

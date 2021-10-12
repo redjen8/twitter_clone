899a37f294e2767f9e611e6e26108f4158a872f6
redjen8 <seokwoo2525@gmail.com>
2021-10-12 22:34:11 +0900

    added sort by created date

--- nwitter/src/routes/Home.js
+++ nwitter/src/routes/Home.js
@@ -10,7 +10,7 @@ const Home = ({userObj}) => {
     const [attachment, setAttachment] = useState("");
 
     useEffect(() => {
-        dbService.collection("nweets").onSnapshot((snapshot) => {
+        dbService.collection("nweets").orderBy("createdAt", "desc").onSnapshot((snapshot) => {
             const newArray = snapshot.docs.map((document) => ({
                 id: document.id,
                 ...document.data(),

2edaf307f890f13bbb12082bfd773b954aec22e1
redjen8 <seokwoo2525@gmail.com>
2021-10-12 22:36:36 +0900

    fixed pic upload bug

--- nwitter/src/components/NweetFactory.js
+++ nwitter/src/components/NweetFactory.js
@@ -54,7 +54,9 @@ const NweetFactory = ({ userObj }) => {
             } = finishedEvent;
             setAttachment(result);
         };
-        reader.readAsDataURL(theFile);
+        if(Boolean(theFile)) {
+            reader.readAsDataURL(theFile);
+        }
     };
 
     const onClearAttachment = () => setAttachment("");
